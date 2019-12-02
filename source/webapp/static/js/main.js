2294fb63cf2be661c6d190729e6c25c286d5de58
tyzo4ka <tyzo4ka@gmail.com>
2019-12-02 18:43:22 +0600

    Added function calculate to all actions on calcalator

--- source/webapp/static/js/main.js
+++ source/webapp/static/js/main.js
@@ -1,65 +1,97 @@
-function hello() {
-    console.log('Tratata')
+function addition() {
+    var b = document.getElementById('B').value;
+    var answer = document.getElementsByClassName("answer")
+    var a = document.getElementById('A').value;
+    $.ajax({
+        url: "http://127.0.0.1:5000/add/",
+        method: 'POST',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({"A": a, "B": b}),
+        success: function (data) {
+            answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`;
+        },
+        error: function (data) {
+            answer[0].innerHTML += `<p style="color: red;">${data.responseJSON.error}</p>`;
+        }
+    });
 }
 
-function addition() {
-     var b = document.getElementById('B').value;
-     var answer = document.getElementsByClassName("answer")
-     var a = document.getElementById('A').value;
-     $.ajax({
-     url: "http://127.0.0.1:5000/add/",
-     method: 'POST',
-     dataType: "json",
-     contentType: "application/json",
-     data: JSON.stringify({"A": a, "B": b}),
-     success: function(data) {answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`;},
-     error: function(data) {answer[0].innerHTML +=`<p style="color: red;">${data.responseJSON.error}</p>`;}
- });
- }
-
 function subtraction() {
-     var b = document.getElementById('B').value;
-     var a = document.getElementById('A').value;
-     var answer = document.getElementsByClassName("answer")
+    var b = document.getElementById('B').value;
+    var a = document.getElementById('A').value;
+    var answer = document.getElementsByClassName("answer")
 
-     $.ajax({
-     url: "http://127.0.0.1:5000/subtract/",
-     method: 'POST',
-     dataType: "json",
-     contentType: "application/json",
-     data: JSON.stringify({"A": a, "B": b}),
-     success: function(data) {answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`},
-     error: function(data) {answer[0].innerHTML +=`<p style="color: red;">${data.responseJSON.error}</p>`;}
- });
- }
+    $.ajax({
+        url: "http://127.0.0.1:5000/subtract/",
+        method: 'POST',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({"A": a, "B": b}),
+        success: function (data) {
+            answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`
+        },
+        error: function (data) {
+            answer[0].innerHTML += `<p style="color: red;">${data.responseJSON.error}</p>`;
+        }
+    });
+}
 
- function multiply() {
-     var b = document.getElementById('B').value;
-     var a = document.getElementById('A').value;
-     var answer = document.getElementsByClassName("answer")
-     $.ajax({
-     url: "http://127.0.0.1:5000/multiply/",
-     method: 'POST',
-     dataType: "json",
-     contentType: "application/json",
-     data: JSON.stringify({"A": a, "B": b}),
-     success: function(data) {answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`},
-     error: function(data) {answer[0].innerHTML +=`<p style="color: red;">${data.responseJSON.error}</p>`;}
- });
- }
+function multiply() {
+    var b = document.getElementById('B').value;
+    var a = document.getElementById('A').value;
+    var answer = document.getElementsByClassName("answer")
+    $.ajax({
+        url: "http://127.0.0.1:5000/multiply/",
+        method: 'POST',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({"A": a, "B": b}),
+        success: function (data) {
+            answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`
+        },
+        error: function (data) {
+            answer[0].innerHTML += `<p style="color: red;">${data.responseJSON.error}</p>`;
+        }
+    });
+}
 
- function divide() {
-     var b = document.getElementById('B').value;
-     var a = document.getElementById('A').value;
-     var answer = document.getElementsByClassName("answer")
+function divide() {
+    var b = document.getElementById('B').value;
+    var a = document.getElementById('A').value;
+    var answer = document.getElementsByClassName("answer")
 
-     $.ajax({
-     url: "http://127.0.0.1:5000/divide/",
-     method: 'POST',
-     dataType: "json",
-     contentType: "application/json",
-     data: JSON.stringify({"A": a, "B": b}),
-     success: function(data) {answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`},
-     error: function(data) {answer[0].innerHTML +=`<p style="color: red;">${data.responseJSON.error}</p>`;}
- });
- }
+    $.ajax({
+        url: "http://127.0.0.1:5000/divide/",
+        method: 'POST',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({"A": a, "B": b}),
+        success: function (data) {
+            answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`
+        },
+        error: function (data) {
+            answer[0].innerHTML += `<p style="color: red;">${data.responseJSON.error}</p>`;
+        }
+    });
+}
+
+function calculate(action) {
+    var b = document.getElementById('B').value;
+    var a = document.getElementById('A').value;
+    var answer = document.getElementsByClassName("answer")
+
+    $.ajax({
+        url: "http://127.0.0.1:5000/" + $(action).data('id') + "/",
+        method: 'POST',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({"A": a, "B": b}),
+        success: function (data) {
+            answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`
+        },
+        error: function (data) {
+            answer[0].innerHTML += `<p style="color: red;">${data.responseJSON.error}</p>`;
+        }
+    });
+}
