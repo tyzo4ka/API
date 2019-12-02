01bff8719f8c5dee6be29e3c372dc6209fde13c9
tyzo4ka <tyzo4ka@gmail.com>
2019-12-02 18:01:47 +0600

    Added succes and error output on display

--- source/webapp/static/js/main.js
+++ source/webapp/static/js/main.js
@@ -4,64 +4,62 @@ function hello() {
 
 function addition() {
      var b = document.getElementById('B').value;
-     console.log(b)
+     var answer = document.getElementsByClassName("answer")
      var a = document.getElementById('A').value;
-     console.log(a)
      $.ajax({
      url: "http://127.0.0.1:5000/add/",
      method: 'POST',
      dataType: "json",
      contentType: "application/json",
      data: JSON.stringify({"A": a, "B": b}),
-     success: function(response) { console.log(response); },
-     error: function(response) { console.log(response); }
+     success: function(data) {answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`;},
+     error: function(data) {answer[0].innerHTML +=`<p style="color: red;">${data.responseJSON.error}</p>`;}
  });
  }
 
 function subtraction() {
      var b = document.getElementById('B').value;
-     console.log(b)
      var a = document.getElementById('A').value;
-     console.log(a)
+     var answer = document.getElementsByClassName("answer")
+
      $.ajax({
      url: "http://127.0.0.1:5000/subtract/",
      method: 'POST',
      dataType: "json",
      contentType: "application/json",
      data: JSON.stringify({"A": a, "B": b}),
-     success: function(response) { console.log(response); },
-     error: function(response) { console.log(response); }
+     success: function(data) {answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`},
+     error: function(data) {answer[0].innerHTML +=`<p style="color: red;">${data.responseJSON.error}</p>`;}
  });
  }
 
  function multiply() {
      var b = document.getElementById('B').value;
-     console.log(b)
      var a = document.getElementById('A').value;
-     console.log(a)
+     var answer = document.getElementsByClassName("answer")
      $.ajax({
      url: "http://127.0.0.1:5000/multiply/",
      method: 'POST',
      dataType: "json",
      contentType: "application/json",
      data: JSON.stringify({"A": a, "B": b}),
-     success: function(response) { console.log(response); },
-     error: function(response) { console.log(response); }
+     success: function(data) {answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`},
+     error: function(data) {answer[0].innerHTML +=`<p style="color: red;">${data.responseJSON.error}</p>`;}
  });
  }
 
  function divide() {
      var b = document.getElementById('B').value;
-     console.log(b)
      var a = document.getElementById('A').value;
-     console.log(a)
+     var answer = document.getElementsByClassName("answer")
+
      $.ajax({
      url: "http://127.0.0.1:5000/divide/",
      method: 'POST',
      dataType: "json",
      contentType: "application/json",
      data: JSON.stringify({"A": a, "B": b}),
-     success: function(response) { console.log(response); },
-     error: function(response) { console.log(response); }
+     success: function(data) {answer[0].innerHTML += `<p style="color: green;">${data.answer}</p>`},
+     error: function(data) {answer[0].innerHTML +=`<p style="color: red;">${data.responseJSON.error}</p>`;}
  });
  }
